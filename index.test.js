67fa10d4de40952e26d2e7c27ad6de9c0df8ba3a
michaelknoch <info@michaelknoch.de>
2018-06-07 19:06:35 +0200

    introduce eslint, fix linting errors
    cleanup circle file
    
    sachen

--- index.test.js
+++ index.test.js
@@ -2,63 +2,58 @@
 jest.mock('NativeModules', () => ({
     RNHomeIndicator: {
         autoHidden: jest.fn(),
-        alwaysVisible: jest.fn()
-    }
-}))
+        alwaysVisible: jest.fn(),
+    },
+}));
+
+/* eslint-disable import/first */
 
 import React from 'react';
 import PrefersHomeIndicatorAutoHidden, { HomeIndicator } from './';
 import renderer from 'react-test-renderer';
-
 import { NativeModules, View } from 'react-native';
+
+/* eslint-enable import/first */
+
 const { RNHomeIndicator } = NativeModules;
 
-describe('react-native-home-indicator', () => {    
+describe('react-native-home-indicator', () => {
     beforeEach(() => {
-        HomeIndicator.propsHistory = []
-        RNHomeIndicator.autoHidden.mockReset(),
-        RNHomeIndicator.alwaysVisible.mockReset()
-    });
-    
-    test('test hide native indicator when mounting PrefersHomeIndicatorAutoHidden', () => {  
-        const component = renderer.create(
-            <PrefersHomeIndicatorAutoHidden />
-        );
-        
-        expect(RNHomeIndicator.autoHidden).toHaveBeenCalled()
+        HomeIndicator.propsHistory = [];
+        RNHomeIndicator.autoHidden.mockReset();
+        RNHomeIndicator.alwaysVisible.mockReset();
     });
 
-    test('test hide native indicator when mounting HomeIndicator with autoHidden=true', () => {  
-        const component = renderer.create(
-            <HomeIndicator autoHidden={true} />
-        );
-        
-        expect(RNHomeIndicator.autoHidden).toHaveBeenCalled()
+    test('test hide native indicator when mounting PrefersHomeIndicatorAutoHidden', () => {
+        renderer.create(<PrefersHomeIndicatorAutoHidden />);
+        expect(RNHomeIndicator.autoHidden).toHaveBeenCalled();
     });
 
-    test('test hide native indicator when mounting HomeIndicator with autoHidden=false', () => {  
-        const component = renderer.create(
-            <HomeIndicator autoHidden={false} />
-        );
-        
-        expect(RNHomeIndicator.autoHidden).not.toHaveBeenCalled()
+    test('test hide native indicator when mounting HomeIndicator with autoHidden=true', () => {
+        renderer.create(<HomeIndicator autoHidden />);
+        expect(RNHomeIndicator.autoHidden).toHaveBeenCalled();
     });
 
-    test('test propsHistory', () => {  
-        const component = renderer.create(
+    test('test hide native indicator when mounting HomeIndicator with autoHidden=false', () => {
+        renderer.create(<HomeIndicator autoHidden={false} />);
+        expect(RNHomeIndicator.autoHidden).not.toHaveBeenCalled();
+    });
+
+    test('test propsHistory', () => {
+        renderer.create(
             <View>
                 <HomeIndicator autoHidden={false} />
-                <HomeIndicator autoHidden={true} />
-                <HomeIndicator autoHidden={true} />
+                <HomeIndicator autoHidden />
+                <HomeIndicator autoHidden />
                 <HomeIndicator autoHidden={false} />
-            </View>
+            </View>,
         );
-        
+
         expect(HomeIndicator.propsHistory).toEqual([
             { autoHidden: false },
             { autoHidden: true },
             { autoHidden: true },
             { autoHidden: false },
-        ])
+        ]);
     });
 });
